05ddb50993c041e355bbcea07c19e212bb7fb4ac
DaidoujiChen <daidoujichen@gmail.com>
2014-03-04 10:45:45 +0800

    Initial Commit

---
+++ LightWeightPlist/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LightWeightPlist
+//
+//  Created by 啟倫 陳 on 2014/3/4.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LightWeightPlistTests/LightWeightPlistTests.m
@@ -0,0 +1,34 @@
+//
+//  LightWeightPlistTests.m
+//  LightWeightPlistTests
+//
+//  Created by 啟倫 陳 on 2014/3/4.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LightWeightPlistTests : XCTestCase
+
+@end
+
+@implementation LightWeightPlistTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
